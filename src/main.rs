362ee59da797006d7c783512e17dec2309d53d1e
Asad Ali <asad.ali@invozone.com>
2023-12-18 22:22:27 +0500

    implementing clap library

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use std::*;
+use clap::Parser; 
+
+
+// search for a pattern in a file and display the lines that contain it. 
+#[derive(Parser)]
+struct Cli {
+
+    // the pattern to look for
+    pattern : String, 
+
+    // the path to the file to read
+    path: std::path::PathBuf, 
 }
+
+
+fn main() {
+
+
+    // The Index 0 will be the name of program
+    let pattern = std::env::args().nth(1).expect("no pattern given"); 
+    let path = std::env::args().nth(2).expect("no path given"); 
+
+    let args = Cli {
+        pattern: pattern, 
+        path: std::path::PathBuf::from(path), 
+    }; 
+
+
+
+    println!("The pattern is {:?} and path is {:?} ", args.pattern, args.path); 
+
+
+
+}   
